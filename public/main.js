b8041ef2d60ed83ab69bef0859a94a51e3d15d32
hwyiing <pics.cbj@gmail.com>
2022-04-05 17:25:26 +0800

    use online lengzaab picture

--- public/main.js
+++ public/main.js
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const start = async() => {
         const mindarThree = new window.MINDAR.IMAGE.MindARThree({
             container: document.querySelector("#my-ar-container"),
-            imageTargetSrc: 'targets.mind',
+            imageTargetSrc: 'targets-online.mind',
 
         });
         const { renderer, scene, camera } = mindarThree;
